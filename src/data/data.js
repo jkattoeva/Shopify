8665f46872db2d1286341509c57bf87cfa2bb897
Janetta Kattoeva <jkattoeva@gmail.com>
2023-06-13 01:30:03 +0600

    change the title name

--- src/data/data.js
+++ src/data/data.js
@@ -1,31 +1,31 @@
 export const categories = [
   {
     id: 1,
-    title: "Hats",
+    title: "Joyful",
     imageUrl:
       "https://i.pinimg.com/564x/86/ee/b9/86eeb94c59bc0a03077ce9dc984c7aae.jpg",
   },
   {
     id: 2,
-    title: "Jackets",
+    title: "Joyful",
     imageUrl:
       "https://i.pinimg.com/564x/58/80/ab/5880abdb3c843700846af9a741a9813a.jpg",
   },
   {
     id: 3,
-    title: "Sneakers",
+    title: "Joyful",
     imageUrl:
       "https://i.pinimg.com/564x/b2/a3/40/b2a3401eaa6d2d911b8709ad05174516.jpg",
   },
   {
     id: 4,
-    title: "Womens",
+    title: "Joyful",
     imageUrl:
       "https://i.pinimg.com/564x/13/8b/f7/138bf70eda6cea7f536c539271f49ceb.jpg",
   },
   {
     id: 5,
-    title: "Mens",
+    title: "Joyful",
     imageUrl:
       "https://i.pinimg.com/564x/9d/fd/6b/9dfd6b9edf0202b86b842e7501a66d2a.jpg",
   },
