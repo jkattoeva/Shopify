1066e0700bbec22bbabd9a2b2d8de0d476c08e07
Janetta Kattoeva <jkattoeva@gmail.com>
2023-06-15 02:39:09 +0600

    add pages to app

---
+++ src/components/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Directory from "./directory/Directory";
+import { categories } from "../../data/data";
+
+import scss from "./Styles.module.scss";
+
+const HomePage = () => {
+  return (
+    <div className={scss.home__page}>
+      <Directory categories={categories} />
+    </div>
+  );
+};
+
+export default HomePage;
