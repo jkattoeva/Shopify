ed78f63979355bb5afa568f3bc9f8ce9ae6ca53c
Janetta Kattoeva <jkattoeva@gmail.com>
2023-06-15 07:10:44 +0600

    modify the footer part of app

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import Header from "./header/Header";
-import Footer from "./footer/Footer";
 import Main from "./main/Main";
-
+import Footer from "./footer/Footer";
 import scss from "./Layout.module.scss";
 
 const Layout = () => {
--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-
-const Foooter = () => {
-  return <div>Foooter</div>;
+import scss from "./Footer.module.scss";
+import { Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <div className={scss.footer}>
+      <Link className={scss.logo} to="/">
+        <img
+          src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/110/8414017110_8e9062f0-6eb2-4950-be7c-f82071b345c6.png?cb=1686784983"
+          alt="logo"
+        />
+      </Link>
+    </div>
+  );
 };
 
-export default Foooter;
+export default Footer;
