53f38ea23cdfedafb194d700d2a173525d61d11a
Janetta Kattoeva <jkattoeva@gmail.com>
2023-06-15 07:02:31 +0600

    change the styles

--- src/components/pages/category/Category.jsx
+++ src/components/pages/category/Category.jsx
@@ -6,7 +6,7 @@ const Category = ({ item }) => {
     <div className={scss.postCard}>
       <img src={item.imageUrl} alt={item.title} />
       <div className={scss.post_elements}>
-        <h1>{item.title}</h1>
+        <h1 className={scss.title}>{item.title}</h1>
         <span className={scss.price}>{item.price}</span>
         <button className={scss.postBtn}>Add To Cart</button>
       </div>
