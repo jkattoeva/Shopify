7100e2ba33f616417de71d77b94c215081c72576
Janetta Kattoeva <jkattoeva@gmail.com>
2023-06-26 04:58:05 +0600

    modify the toggle part of navbar

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,28 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
 import scss from "./Header.module.scss";
 
 const Header = () => {
   const [active, setActive] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
-  window.addEventListener("scroll", () => {
-    if (window.scrollY >= 60) {
-      setActive(true);
-    } else {
-      setActive(false);
-    }
-  });
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 60) {
+        setActive(true);
+      } else {
+        setActive(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const toggleIcon = () => {
+    setToggle(!toggle);
+  };
 
   return (
-    <div className={active ? scss.header_active : scss.header}>
+    <div className={`${active ? scss.header_active : scss.header}`}>
       <Link className={scss.logo} to="/">
         <img
           src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/110/8414017110_8e9062f0-6eb2-4950-be7c-f82071b345c6.png?cb=1686784983"
           alt="logo"
         />
       </Link>
-      <div className={scss.nav}>
+      <Link
+        onClick={toggleIcon}
+        className={`${toggle ? scss.burger : scss.nav_vis}`}
+      >
+        Burger
+      </Link>
+      <div className={`${scss.nav} ${toggle ? scss.nav_vis : ""}`}>
         <Link className={scss.link} to="/">
           Home
         </Link>
