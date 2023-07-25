a365931da1267e3f7256bc7180af002b274e1c2e
Janetta Kattoeva <jkattoeva@gmail.com>
2023-07-26 03:27:06 +0600

    make headcontent as props

--- src/components/pages/headContent/HeadContent.jsx
+++ src/components/pages/headContent/HeadContent.jsx
@@ -3,22 +3,38 @@ import walp from "../../../assets/images/heyheyhey.jpg";
 import walpVercage from "../../../assets/images/joji.jpg";
 import scss from "./HeadContent.module.scss";
 
-const HeadContent = () => {
+const HeadContent = ({ page }) => {
   return (
     <div className={scss.head__content}>
-      <div className={scss.container}>
-        <div className={scss.text__content}>
-          <p>Have a nice joy with us</p>
-        </div>
-        <div className={scss.images}>
-          <div className={scss.image}>
-            <img src={walpVercage} alt="walp" />
+      {page === "home" ? (
+        <div className={scss.container}>
+          <div className={scss.text__content}>
+            <p>Have a nice joy with us</p>
           </div>
-          <div className={scss.image}>
-            <img src={walp} alt="walp" />
+          <div className={scss.images}>
+            <div className={scss.image}>
+              <img src={walpVercage} alt="walp" />
+            </div>
+            <div className={scss.image}>
+              <img src={walp} alt="walp" />
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className={scss.container}>
+          <div className={scss.text__content}>
+            <p>Contact us</p>
+          </div>
+          <div className={scss.images}>
+            <div className={scss.image}>
+              <img src={walpVercage} alt="walp" />
+            </div>
+            <div className={scss.image}>
+              <img src={walp} alt="walp" />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
