b528c03413ab62e4353de78d20adc4820a7dd3c4
Janetta Kattoeva <jkattoeva@gmail.com>
2023-07-26 03:25:14 +0600

    add contact page content

--- src/components/pages/contact/Contact.jsx
+++ src/components/pages/contact/Contact.jsx
@@ -4,7 +4,28 @@ import scss from "./Contact.module.scss";
 const Contact = () => {
   return (
     <div className={scss.page}>
-      <input name="name" type="text" />
+      <div className={scss.title}>
+        <h1>Contact Us Dear Bro 🙌🏻</h1>
+      </div>
+      <form className={scss.form}>
+        <div className={scss.input_wrapper}>
+          <span>Your Full Name</span>
+          <input className={scss.input} name="name" type="text" />
+        </div>
+        <div className={scss.input_wrapper}>
+          <span>Your Number</span>
+          <input className={scss.input} name="name" type="text" />
+        </div>
+        <div className={scss.input_wrapper}>
+          <span>Your Message</span>
+          <textarea
+            className={scss.input}
+            name="name"
+            placeholder="type here..."
+            type="text"
+          />
+        </div>
+      </form>
     </div>
   );
 };
