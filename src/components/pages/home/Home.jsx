cf48809a81d5878888dff9875c0046f756b32b1b
Janetta Kattoeva <jkattoeva@gmail.com>
2023-07-25 22:25:25 +0600

    modify several folders

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Directory from "../directory/Directory";
 import { categories } from "../../../data/data";
+import HeadContent from "../headContent/HeadContent";
 import scss from "../Styles.module.scss";
 
 const Home = () => {
   return (
     <div className={scss.home__page}>
+      <HeadContent />
       <Directory categories={categories} />
     </div>
   );
