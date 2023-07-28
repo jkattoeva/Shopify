6c022c03a1cb0c22e9887527fe0af5f2ba8ec160
Janetta Kattoeva <jkattoeva@gmail.com>
2023-07-28 23:43:16 +0600

    modify the about page

--- src/components/pages/about/About.jsx
+++ src/components/pages/about/About.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import image from "../../../assets/images/background.webp";
 import scss from "./About.module.scss";
 
@@ -6,24 +5,53 @@ const About = () => {
   return (
     <div className={scss.about__page}>
       <div className={scss.container}>
-        <div className={scss.text}>
-          <h1 className={scss.title}>About us</h1>
-          <p className={scss.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
-            ab deserunt blanditiis sint, nam exercitationem aliquid illo neque
-            porro, soluta deleniti. Vitae vero in placeat eius quas accusamus
-            incidunt asperiores.
-          </p>
+        <div className={scss.title}>
+          <h1>
+            Belive that you find your style Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Veniam dolore sunt provident, dolorum
+            iusto amet ipsa autem, fugit quidem mollitia soluta, sapiente illo
+            perspiciatis. Quos pariatur dolorem voluptatibus id molestiae?
+          </h1>
         </div>
-        <div className={scss.images}>
-          <div className={scss.image}>
-            <img src={image} alt="img" />
+        <div className={scss.scnd_cont}>
+          <div className={scss.img_desc}>
+            <div className={scss.image}>
+              <img src={image} alt="image" />
+            </div>
+            <div className={scss.desc}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Mollitia atque debitis consectetur animi, corporis id laborum
+                quae blanditiis assumenda. Distinctio nulla repudiandae
+                molestias sunt magni optio harum laborum inventore aliquid.
+              </p>
+            </div>
           </div>
-          <div className={scss.image}>
-            <img src={image} alt="img" />
+          <div className={scss.img_desc}>
+            <div className={scss.image}>
+              <img src={image} alt="image" />
+            </div>
+            <div className={scss.desc}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Mollitia atque debitis consectetur animi, corporis id laborum
+                quae blanditiis assumenda. Distinctio nulla repudiandae
+                molestias sunt magni optio harum laborum inventore aliquid.
+              </p>
+            </div>
           </div>
-          <div className={scss.image}>
-            <img src={image} alt="img" />
+          <div className={scss.img_desc}>
+            <div className={scss.image}>
+              <img src={image} alt="image" />
+            </div>
+            <div className={scss.desc}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Mollitia atque debitis consectetur animi, corporis id laborum
+                quae blanditiis assumenda. Distinctio nulla repudiandae
+                molestias sunt magni optio harum laborum inventore aliquid.
+              </p>
+            </div>
           </div>
         </div>
       </div>
